6a6dcca064aac02df050eea071de38a37c8b6cb6
humzasandhu <humzasandhu736@gmail.com>
2024-01-28 00:39:51 -0500

    initial start

---
+++ src/pages/home.jsx
@@ -0,0 +1,14 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+export default function HomePage() {
+    return(
+        <Container>
+            <Row>
+                <Col>
+                    <h1>Ballar Sells Movies</h1>
+                </Col>
+            </Row>
+        </Container>
+    );
+}
