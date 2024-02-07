ab59abd350d0038dadef6adb2298c5c12468de4a
humzasandhu <humzasandhu736@gmail.com>
2024-02-07 02:31:16 -0500

    hola

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,4 @@
-import { Row, Form, InputGroup, Button, Col, Table, Spinner } from 'react-bootstrap';
+import { Modal, Row, Form, InputGroup, Button, Col, Table, Spinner, ListGroup } from 'react-bootstrap';
 import { useState, useEffect } from 'react';
 
 export default function SearchBar({ query }) {
@@ -6,15 +6,17 @@ export default function SearchBar({ query }) {
     const [searchResults, setSearchResults] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [filterByActor, setFilterByActor] = useState(false);
-    const [filterByGenra, setFilterByGenra] = useState(false);
+    const [filterByGenre, setFilterByGenre] = useState(false);
     const [modal, setShowModal] = useState(false);
+    const [selectedFilm, setSelectedFilm] = useState(null);
+    const [filmDetails, setFilmDetails] = useState({});
 
     useEffect(() => {
-        if (movieTitle || filterByActor || filterByGenra) { 
+        if (movieTitle || filterByActor || filterByGenre) {
             setIsLoading(true);
             fetchSearchResults();
         }
-    }, [movieTitle, filterByActor, filterByGenra]);
+    }, [movieTitle, filterByActor, filterByGenre]);
 
     const handleSearch = () => {
         fetchSearchResults();
@@ -26,9 +28,9 @@ export default function SearchBar({ query }) {
         }
     };
 
-    const handleGenraFilter = async () => {
-        if (movieTitle || filterByGenra) {
-            setFilterByGenra(!filterByGenra);
+    const handleGenreFilter = async () => {
+        if (movieTitle || filterByGenre) {
+            setFilterByGenre(!filterByGenre);
         }
     };
 
@@ -39,7 +41,7 @@ export default function SearchBar({ query }) {
         if (!inputValue) {
             setMovieTitle('');
             setFilterByActor(false);
-            setFilterByGenra(false);
+            setFilterByGenre(false);
             setSearchResults([]);
         } else {
             setMovieTitle(inputValue);
@@ -51,14 +53,14 @@ export default function SearchBar({ query }) {
             let apiUrl = '';
             if (filterByActor) {
                 apiUrl = `http://localhost:3001/searchMoviesActor/${encodeURIComponent(movieTitle)}`;
-            } else if (filterByGenra) {
-                apiUrl = `http://localhost:3001/searchMoviesGenra/${encodeURIComponent(movieTitle)}`;
+            } else if (filterByGenre) {
+                apiUrl = `http://localhost:3001/searchMoviesGenre/${encodeURIComponent(movieTitle)}`;
             } else {
                 apiUrl = `http://localhost:3001/searchMoviesTitle/${encodeURIComponent(movieTitle)}`;
             }
 
             const response = await fetch(apiUrl);
-            
+
             // Check if the response status is OK (200)
             if (!response.ok) {
                 throw new Error(`HTTP error! Status: ${response.status}`);
@@ -79,71 +81,128 @@ export default function SearchBar({ query }) {
         }
     };
 
-    const toggle = () => setShowModal(!modal);
+    ///////////////////////////////////////////////////////////////////////////
+    const showMovieModal = async (film) => {
+        setSelectedFilm(film);
+        setShowModal(true);
+        try {
+            const response = await fetch(`http://localhost:3001/modalFilmDetails/${film.film_id}`);
+            const data = await response.json();
+            setFilmDetails(data);
+        } catch (error) {
+            console.error('Error fetching film details:', error);
+        }
+
+    };
+
+    const FilmDetailsModal = () => {
+        return (
+            <Modal size="lg" centered show={modal} onHide={handleCloseModal}>
+                <Modal.Header closeButton>
+                    <Modal.Title>{filmDetails.title || 'Loading...'}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    {filmDetails.description ? (
+                        <Row className='align-items-center'>
+                            <Col>
+                                <h5>Description:</h5> {filmDetails.description}
+                                <h5 className='my-2'>Cast: </h5>{filmDetails.actor_names}
+                                <p className='mt-2'><b>Special Features:</b> {filmDetails.special_features}</p>
+                            </Col>
+                            <Col>
+                            <ListGroup variant="flush">
+                                <ListGroup.Item><b>Release Year</b> {filmDetails.release_year}</ListGroup.Item>
+                                <ListGroup.Item><b>Genre</b> {filmDetails.name}</ListGroup.Item>
+                                <ListGroup.Item><b>Rating</b> {filmDetails.rating}</ListGroup.Item>
+                                <ListGroup.Item><b>Rental Rate</b> <span className="text-success">${filmDetails.rental_rate}</span></ListGroup.Item>
+                            </ListGroup>
+                                
+                            </Col>
+                        </Row>
+                    ) : (
+                        <p>Loading film details...</p>
+                    )}
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleCloseModal}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        );
+    }
+
+    const handleCloseModal = () => setShowModal(false);
+
 
     return (
-        <Row className='justify-content-center'>
-            <Col md={8}>
-                <Form>
-                    <InputGroup size="lg" className="m-2 align-items-center">
-                        <Form.Control
-                            id='filmSearchBar'
-                            placeholder="Film Search"
-                            aria-label="Search Movies"
-                            aria-describedby="movie-search-input"
-                            value={movieTitle}
-                            onChange={handleInputChange}
-                        />
-                        <Form.Check
-                            type="checkbox"
-                            label="Filter by Actor"
-                            className='mx-4'
-                            id="actorFilter"
-                            checked={filterByActor}
-                            onChange={handleActorFilter}
-                        />
-                        <Form.Check
-                            type="checkbox"
-                            label="Filter by genra"
-                            className='mx-4'
-                            id="actorGenra"
-                            checked={filterByGenra}
-                            onChange={handleGenraFilter}
-                        />
-                        <Button variant="outline-danger" id="search-movies-button" onClick={handleSearch}>
-                            Find
-                        </Button>
-                    </InputGroup>
-                </Form>
-                <div>
-                    {isLoading && <Spinner animation="border" role="status">
-                        <span className="sr-only">Loading...</span>
-                    </Spinner>}
-
-                    {searchResults.length > 0 && (
-                        <Table striped bordered hover>
-                            <thead>
-                                <tr>
-                                    <th>Id</th>
-                                    <th>Title</th>
-                                    <th>Release Year</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {searchResults.map((movie, index) => (
-                                    <tr key={index}>
-                                        <td>{movie.film_id}</td>
-                                        <td>{movie.title}</td>
-                                        <td>{movie.release_year}</td>
+        <>
+            <h1 className="text-danger my-2">Search Movies</h1>
+            <Row className='justify-content-center'>
+                <Col>
+                    <Form className='pt-2 pb-4'>
+                        <InputGroup size="lg" className="align-items-center">
+                            <Form.Control
+                                id='filmSearchBar'
+                                placeholder="Film Search"
+                                aria-label="Search Movies"
+                                aria-describedby="movie-search-input"
+                                value={movieTitle}
+                                onChange={handleInputChange}
+                            />
+                            <Form.Check
+                                type="checkbox"
+                                label="Filter by Actor"
+                                className='mx-4'
+                                id="actorFilter"
+                                checked={filterByActor}
+                                onChange={handleActorFilter}
+                            />
+                            <Form.Check
+                                type="checkbox"
+                                label="Filter by genre"
+                                className='mx-4'
+                                id="actorGenre"
+                                checked={filterByGenre}
+                                onChange={handleGenreFilter}
+                            />
+                            <Button variant="outline-danger" id="search-movies-button" onClick={handleSearch}>
+                                Find
+                            </Button>
+                        </InputGroup>
+                    </Form>
+                    <div>
+                        {isLoading && <Spinner animation="border" role="status">
+                            <span className="sr-only">Loading...</span>
+                        </Spinner>}
+                        {searchResults.length > 0 && (
+                            <Table striped bordered hover>
+                                <thead className='h4'>
+                                    <tr>
+                                        <th>Id</th>
+                                        <th>Title</th>
+                                        <th className='text-center'>Release Year</th>
                                     </tr>
-                                ))}
-                            </tbody>
-                        </Table>
-                    )}
-
-                    {!isLoading && searchResults.length === 0 && <p>No results found.</p>}
-                </div>
-            </Col>
-        </Row>
+                                </thead>
+                                <tbody>
+                                    {searchResults.map((film, index) => (
+                                        <tr key={index} className='align-items-center'>
+                                            <td>{film.film_id}</td>
+                                            <td className='text-danger h6'>{film.title}</td>
+                                            <td className='text-center'>{film.release_year}</td>
+                                            <td className='text-center'>
+                                                <Button variant="outline-danger" onClick={() => showMovieModal(film)}>View Details</Button>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </Table>
+                        )}
+                        {!isLoading && searchResults.length === 0 && <p>No results found.</p>}
+                    </div>
+                </Col>
+            </Row>
+            <FilmDetailsModal></FilmDetailsModal>
+        </>
     );
 }
